57689c95850d1a6209a4f5b6a9e04e12aed93f3f
mauricioganu <54649732+mauricioganu@users.noreply.github.com>
2019-11-16 22:02:07 -0300

    Se agrego css en listado

--- libros/FormEditar.php
+++ libros/FormEditar.php
@@ -1,3 +1,45 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../favicon.ico">
+
+    <title>Entre y salga despacio S.A</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/floating-labels.css" rel="stylesheet">
+
+  </head>
+  <style>
+   
+    th 
+    {
+      color:green;
+      background-color: orange;
+    }
+    td {color:black;}
+    table,th,td 
+    {
+     border: 10px solid black;
+    text-align: center;
+    }
+
+    </style>  
+    <body>
+
+    <header>
+    	<?php
+        include "componentes/menu.php";
+    ?>
+    </header>
 
 <?php
 include("conexion.php");
@@ -18,6 +60,7 @@ if($_GET) {
 <!-- Formulario para modificar los datos -->
 <HTML>
 <Body>
+	 <main role="main" class="container">
 <form action="Edito.php"  metod="get">
 <p name="titulo">Titulo:<?php echo $f_titulo; ?></p>
 <br>
@@ -37,5 +80,24 @@ if($_GET) {
 <br>
  <input type="submit" value="Modificar">
 </form> 
+</main>
+      
+     <footer class="footer">
+    <?php
+        include "componentes/pie.php";
+    ?>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" cAlumnorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+	
  </Body>
  </HTML>
--- libros/listado.php
+++ libros/listado.php
@@ -1,3 +1,68 @@
+
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../favicon.ico">
+
+    <title>Entre y salga despacio S.A</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/floating-labels.css" rel="stylesheet">
+
+  </head>
+
+
+  <style>
+   
+    th 
+    {
+      color:green;
+      background-color: orange;
+    }
+    td {color:black;}
+    table,th,td 
+    {
+     border: 10px solid black;
+    text-align: center;
+    }
+
+    </style>  
+
+  <body>
+
+    <header>
+    	<?php
+        include "componentes/menu.php";
+    ?>
+    </header>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+         
+    
+
+
+
+      <table style="width:100%">
+
+       <tr>
+            <th>Autor</th>
+            <th>Titulo</th>
+          	<th>Precio</th>
+            <th>Genero</th>
+            <th>Cantidad</th>
+
+          </tr>
+
 <?php
 
 include("conexion.php");
@@ -11,27 +76,6 @@ $query= 'SELECT * FROM libros WHERE autor="'.$f_autor.'"';
 $suma=0;
 $sumatotal=0;
 
-
-
-// Listamos sólo la zona seleccionada
-echo "<table border='2'>";
-echo "<tr>";
-echo "<th>Autor</th>";
-
-echo "<th>Titulo</th>";
-
-echo "<th>Precio</th>";
-
-echo "<th>Genero</th>";	
-
-echo "<th>Cantidad</th>";
-
-
-
-
-
-
-echo "</tr>";
 while($fila = mysqli_fetch_array($resultados)){
 	echo "<tr>";
 	echo "<td>";
@@ -64,7 +108,25 @@ echo"<tr><td>Subtotal</td><td>"."$".$sumatotal."</td></tr>";
 }
 echo "</table>";
 echo "<br>";
-echo "<a href='index.php'><button type='button'>VOLVER</button></a>";
+
  $conn->close();
 ?>
 
+ </main>
+      
+     <footer class="footer">
+    <?php
+        include "componentes/pie.php";
+    ?>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" cAlumnorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
